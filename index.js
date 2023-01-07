b80d2af824d193e5b24689b4a099e331dd062726
Arjunsingh Rajput <asr9320003120@gmail.com>
2023-01-07 16:01:40 +0530

    final commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let outputBox = document.querySelector("#output-box");
 
 let wikipediaUrl = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20`;
 
-const constructedURl = (searchVal) => {
+const constructedURL = (searchVal) => {
   return `${wikipediaUrl}&srsearch=${searchVal}`;
 }
 
